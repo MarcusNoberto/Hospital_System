2718e78697ef4e0c6671b5ce7691df8cdbde9911
MarcusNoberto <marcus.ideao@ccc.ufcg.edu.br>
2022-05-05 20:44:01 -0300

    first admin doctor and patient view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
+from . import forms
+from django.db.models import Sum
+from django.contrib.auth.models import Group
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required,user_passes_test
+from datetime import datetime,timedelta,date
+from django.conf import settings
 
-# Create your views here.
+def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('AfterLogin')
+
+    return render(request, 'Hospital/index.html')
+
+def admin_click_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('AfterLogin')
+
+    return render(request, 'Hospital/adminClick.html')
+
+def doctorclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'hospital/doctorclick.html')
+
+def patientclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'hospital/patientclick.html')
+
+
+def admin_signup_view(request):
+    form = forms.AdminSignupForm()
+    if request.method == "POST":
+        form = forms.AdminSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.set_password(user.password)
+            user.save()
+            my_admin_group = Group.objects.get_or_create(name = 'ADMIN')
+            my_admin_group[0].user_set.add(user)
+            return HttpResponseRedirect('adminLogin')
+
+    return render(request,'Hospital/adminSignup.html')
+
+def doctor_signup_view(request):
+    userForm = forms.DoctorUserForm()
+    doctorForm = forms.DoctorForm()
+    my_dict = {'userForm':userForm, 'doctorForm': doctorForm}
+    if request.method == 'POST':
+        userForm = forms.DoctorUserForm(request)
+        doctorForm = forms.DoctorForm(request.POST, request.FILES )
+        if userForm.is_valid() and doctorForm.is_valid():
+            user = userForm.save()
+            user.set_password(user.password)
+            user.save()
+            doctor = doctorForm.save(commit=False)
+            doctor.user = user
+            doctor = doctor.save()
+            my_doctor_group = Group.objects.get_or_create(name='DOCTOR')
+            my_doctor_group[0].user_set.add(user)
+
+        return HttpResponseRedirect('doctorLogin')
+    return render(request,'hospital/doctorsignup.html',context=my_dict)
+
+def patient_signup_view(request):
+    userForm=forms.PatientUserForm()
+    patientForm=forms.PatientForm()
+    mydict={'userForm':userForm,'patientForm':patientForm}
+    if request.method=='POST':
+        userForm=forms.PatientUserForm(request.POST)
+        patientForm=forms.PatientForm(request.POST,request.FILES)
+        if userForm.is_valid() and patientForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            patient=patientForm.save(commit=False)
+            patient.user=user
+            patient.assignedDoctorId=request.POST.get('assignedDoctorId')
+            patient=patient.save()
+            my_patient_group = Group.objects.get_or_create(name='PATIENT')
+            my_patient_group[0].user_set.add(user)
+        return HttpResponseRedirect('patientLogin')
+    return render(request,'hospital/patientsignup.html',context=mydict)
+
